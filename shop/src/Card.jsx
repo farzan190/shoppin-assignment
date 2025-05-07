018b3fe6d21eecc5f375c1b33cd4e67c0e62b20e
farzan190 <farzanxavier@gmail.com>
2025-05-07 21:22:49 +0530

    added upward and downward swipe actions

--- shop/src/Card.jsx
+++ shop/src/Card.jsx
@@ -11,20 +11,32 @@ const Card = ({  setData,name, brand, price, originalPrice, discountPercentage, 
 
       console.log("Swiped left");
     }
+    else if (direction === "up") {
+      setData((prevData) => prevData.slice(0, -1)); 
+
+      console.log("Swiped up");
+    }
+    else if (direction === "down") {
+      setData((prevData) => prevData.slice(0, -1)); 
+
+      console.log("Swiped down");
+    }
   };
 
   return (
     <motion.div
-      drag="x"
-      dragConstraints={{ left: -0, right: 0 }}  
-      onDragEnd={(e, info) => {
-        if (info.offset.x > 100) handleSwipe("right");
-        else if (info.offset.x < -100) handleSwipe("left");
-      }}
+      drag="x y"
+      dragConstraints={{ top: 0, bottom: 0, left: 0, right: 0 }}
+  onDragEnd={(e, info) => {
+    if (info.offset.x > 100) handleSwipe("right");
+    else if (info.offset.x < -100) handleSwipe("left");
+    else if (info.offset.y < -100) handleSwipe("up");
+    else if (info.offset.y > 100) handleSwipe("down");
+  }}
       initial={{ scale: 1 }}
       animate={{ x: 0, opacity: 1 }}
       exit={{ x: 300, opacity: 0 }}  
-      transition={{ duration: 0.1 }}
+      transition={{ duration: 0.2 }}
       style={{
         position: 'absolute',   
         width: '100%',
