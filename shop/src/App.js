321e756b755cac2192b1dfd7e282d3905cb150b0
farzan190 <farzanxavier@gmail.com>
2025-05-07 18:44:01 +0530

    added swipe feature

--- shop/src/App.js
+++ shop/src/App.js
@@ -2,16 +2,13 @@ import './App.css';
 import  {useState} from 'react';
 import data from './data';
 import Card from './Card';
-import Draggable from 'react-draggable';
 
 function App() {
   const[products,setProducts]=useState(data);
- 
+    
   
-  
-return (
-<div className='shoppin'>{products.map((item)=><Card name={item.name} brand={item.brand}  price={item.price} originalPrice={item.originalPrice} discountPercentage={item.discountPercentage} imageUrl={item.imageUrl}/>)}</div>
-       )    
+return <div className='shoppin'>{products.map((item)=><Card  name={item.name} brand={item.brand}  price={item.price} originalPrice={item.originalPrice} discountPercentage={item.discountPercentage} imageUrl={item.imageUrl}/>)}</div>
+           
 
 }
 
--- shop/src/Card.jsx
+++ shop/src/Card.jsx
@@ -1,15 +1,47 @@
-import Draggable from 'react-draggable';
+import { motion } from 'framer-motion';
 
+const Card = ({  name, brand, price, originalPrice, discountPercentage, imageUrl }) => {
+  
+  const handleSwipe = (direction) => {
+    if (direction === "right") {
+      console.log("Swiped right");
+    } else if (direction === "left") {
+      console.log("Swiped left");
+    }
+  };
 
-const Card=({id,name,brand,price,originalPrice,discountPercentage,imageUrl})=>{
-    return <div className="card">
-    <img src={imageUrl}/>
-    <div>{name}</div>
-    <div>{brand}</div>
-    <div>{price}</div>
-    <div>{originalPrice}</div>
-    <div>{discountPercentage}</div>
-    </div>
-}
+  return (
+    <motion.div
+      drag="x"
+      dragConstraints={{ left: -30, right: 30 }}  
+      onDragEnd={(e, info) => {
+        if (info.offset.x > 100) handleSwipe("right");
+        else if (info.offset.x < -100) handleSwipe("left");
+      }}
+      initial={{ scale: 1 }}
+      animate={{ x: 0, opacity: 1 }}
+      exit={{ x: 300, opacity: 0 }}  
+      transition={{ duration: 0.1 }}
+      style={{
+        position: 'absolute',   
+        width: '100%',
+        height: '100%',
+        background: 'transparent',
+        borderRadius: '10px',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >  <div className='card'>
+      <img src={imageUrl} alt={name} />
+      <div>{name}</div>
+      <div>{brand}</div>
+      <div>{price}</div>
+      <div>{originalPrice}</div>
+      <div>{discountPercentage}</div>
+      </div>
+    </motion.div>
+  );
+};
 
 export default Card;
