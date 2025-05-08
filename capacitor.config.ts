d17d13a4f6dab39b157441c3421b53e7b7e99ceb
farzan190 <farzanxavier@gmail.com>
2025-05-08 13:31:57 +0530

    removed node module

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.shoppin.app',
+  appName: 'shoppinApp',
+  webDir: 'build'
+};
+
+export default config;
