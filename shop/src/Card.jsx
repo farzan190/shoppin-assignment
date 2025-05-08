1e301f3f31e45adcc23327176b0fec35abf3c650
farzan190 <farzanxavier@gmail.com>
2025-05-08 12:24:54 +0530

    fixed sideways swipe

--- shop/src/Card.jsx
+++ shop/src/Card.jsx
@@ -25,38 +25,52 @@ const Card = ({  setData,name, brand, price, originalPrice, discountPercentage, 
 
   return (
     <motion.div
-      drag="x y"
-      
+  drag="x"
   onDragEnd={(e, info) => {
     if (info.offset.x > 100) handleSwipe("right");
     else if (info.offset.x < -100) handleSwipe("left");
-    else if (info.offset.y < -100) handleSwipe("up");
-    else if (info.offset.y > 100) handleSwipe("down");
   }}
-      initial={{ scale: 1 }}
-      animate={{ x: 0, opacity: 1 }}
-      exit={{ x: 300, opacity: 0 }}  
-      transition={{ duration: 0.2 }}
-      style={{
-        position: 'absolute',   
-        width: '100%',
-        height: '100%',
-        background: 'transparent',
-        borderRadius: '10px',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        touchAction: 'none',
-      }}
-    >  <div className='card'>
+  style={{
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
+    background: 'transparent',
+    borderRadius: '10px',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    touchAction: 'none',
+  }}
+>
+  <motion.div
+    drag="y"
+    onDragEnd={(e, info) => {
+      if (info.offset.y < -100) handleSwipe("up");
+      else if (info.offset.y > 100) handleSwipe("down");
+    }}
+    style={{
+      position: 'absolute',
+      width: '100%',
+      height: '100%',
+      background: 'transparent',
+      borderRadius: '10px',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      touchAction: 'none',
+    }}
+  >
+    <div className='card'>
       <img src={imageUrl} alt={name} />
       <div>{name}</div>
       <div>{brand}</div>
       <div>{price}</div>
       <div>{originalPrice}</div>
       <div>{discountPercentage}</div>
-      </div>
-    </motion.div>
+    </div>
+  </motion.div>
+</motion.div>
+
   );
 };
 
